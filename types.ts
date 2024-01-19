5c11827ee2376614b80c7531622260818567691d
IgnacioMurubeCrego <imurubec@alumnos.nebrija.es>
2024-01-19 13:56:14 +0100

    Update

--- types.ts
+++ types.ts
@@ -1,23 +1,3 @@
-export type Teacher = {
-  id: string;
-  name: string;
-  email: string;
-  subjects: Array<Omit<Subject, "teacher" | "students">>;
-};
-
-export type Subject = {
-  id: string;
-  name: string;
-  year: string;
-  // Este type es del cliente, por tanto un obj Teacher sin sus 'subjects'
-  // De esta forma evitamos redundancias.
-  teacher?: Omit<Teacher, "subjects">; 
-  students: Array<Omit<Student, "subjects">>;
-};
-
-export type Student = {
-  id: string;
-  name: string;
-  email: string;
-  subjects: Array<Omit<Subject, "students" >>;
-};
+export type Object = {
+  
+}
