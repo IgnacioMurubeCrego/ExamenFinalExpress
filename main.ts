4bb3dc161f4854051e605ef37c4c7bb0db47e0b7
IgnacioMurubeCrego <imurubec@alumnos.nebrija.es>
2024-07-05 12:04:05 +0200

    uptdate

--- main.ts
+++ main.ts
@@ -17,6 +17,6 @@ app
   .get("/", async (req: Request, res: Response) => {})
   
 
-app.listen(3000, () => {
-  console.log("Server listening on port 3000");
+app.listen(4000, () => {
+  console.log("Server listening on port 4000");
 });
